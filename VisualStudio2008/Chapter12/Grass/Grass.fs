c5556ac813329228ce1eb1a486b16e040f0446ff
rwright@starstonesoftware.com <rwright@starstonesoftware.com@36620312-f518-11de-9bdd-55956ae4c37b>
2010-04-13 13:30:10 +0000

    Add field of grass instancing example.

---
+++ VisualStudio2008/Chapter12/Grass/Grass.fs
@@ -0,0 +1,18 @@
+// Perspective correction demonstration
+// Fragment Shader
+// Graham Sellers
+// OpenGL SuperBible
+#version 150
+
+precision highp float;
+
+in vec4 color;
+
+out vec4 output_color;
+
+void main(void)
+{
+    vec2 coord;
+
+    output_color = color;
+}
